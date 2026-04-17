a878c7f552bba6f15edc0115f3a2f682a48b88f9
Z R D ∆ <swdefrr10@gmail.com>
2026-04-17 23:40:15 +0200

    Create username_lookup function for social media links
    Add a function to generate username report links for social media.

---
+++ modules/username.py
@@ -0,0 +1,17 @@
+def username_lookup(username):
+
+    return f"""
+USERNAME REPORT
+
+Instagram:
+https://instagram.com/{username}
+
+GitHub:
+https://github.com/{username}
+
+TikTok:
+https://tiktok.com/@{username}
+
+Twitter:
+https://x.com/{username}
+"""
